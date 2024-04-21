dce969693aa3f8ece1726bd0e907043e3d785154
churt <charliepapa2@gmail.com>
2024-04-21 18:38:34 +0100

    first commit

---
+++ src/chapter1/ex1.1.py
@@ -0,0 +1,9 @@
+"""
+# Is Unique: implement an algorithm to determine if a string has all unique characters.
+What is you cannot use additional data structures?
+"""
+def isUnique(s: str) -> bool:
+    print('running solution...')
+    return False
+
+isUnique('hi');
