13ace9f646a332f289eb2f2fd0d85e383a40a402
churt <charliepapa2@gmail.com>
2024-04-24 10:33:19 +0100

    1.1 using sort

--- src/chapter1/ex1.1.py
+++ src/chapter1/ex1.1.py
@@ -2,8 +2,10 @@
 # Is Unique: implement an algorithm to determine if a string has all unique characters.
 What is you cannot use additional data structures?
 """
-# first implementation
+
 """
+First Implementation
+
 First ask, is the string ASCII or Unicode? We'll assume ASCII. If not we'd need to increase storage size.
 Of course all this assumes I'm using something like c where strings are arrays where memory is allocated
 statically. 
@@ -21,7 +23,6 @@ O(min(c, s)) where c = length of alphabet, s = length of string
 Complexity in space: O(1), alphabet size is constant.
 """
 def isUnique(s: str) -> bool:
-    print('running solution for ', s)
     length_alphabet = 2
     if len(s) > length_alphabet:
         # the string has at least 1 of every character and so is not unique
@@ -43,8 +44,46 @@ print(isUnique('bb'))
 
 """
 Implementation using a bit vector
+essentially we'd have an int with as many bits as there are characters
+then we'd assign a bit to each character based on a position in the bit.
+So for example a=1st bit (least significant), b= second bit, etc.
+The int is initialized at 0, and the bits are marked. In order to do this
+in python though, you'd need to get a way to get ASCII codes to get the
+character's position. So, a=100, b=101, etc. for example.
 """
 
 """
 Implementation using no additional data structures
+Complexity in time: O(n) where n = length of string
+Complexity in space: O(1)
 """
+def isUniqueNoHash(s: str) -> bool:
+    sorted_s = sorted(s)
+    for i in range(1, len(s)):
+        if sorted_s[i-1] == sorted_s[i]:
+            return False
+    return True
+print('======== No hash ========')
+print(isUniqueNoHash('a'))
+print(isUniqueNoHash('b'))
+print(isUniqueNoHash(''))
+print(isUniqueNoHash('ab'))
+print(isUniqueNoHash('aa'))
+print(isUniqueNoHash('bb'))
+
+"""
+Implementation using no additional data structures
+and without modifying the array.
+Complexity in time: 
+Complexity in space: 
+"""
+def isUniqueNoHashPure(s: str) -> bool:
+
+    return True
+print('======== No hash pure ========')
+print(isUniqueNoHashPure('a'))
+print(isUniqueNoHashPure('b'))
+print(isUniqueNoHashPure(''))
+print(isUniqueNoHashPure('ab'))
+print(isUniqueNoHashPure('aa'))
+print(isUniqueNoHashPure('bb'))
